8ea53466f3318bf881852af588669437af9036ad
Adele <56159423+Adeleimpa@users.noreply.github.com>
2023-04-09 12:39:29 +0200

    sign up warning when empty name/email
    NOTE: interface bug mais ca depend sur quel tel on teste, ca me rend triste

--- GestInterim/app/src/main/java/com/example/gestinterim/MainActivity.java
+++ GestInterim/app/src/main/java/com/example/gestinterim/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.gestinterim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { // Login activity
+
+    Button signup_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signup_button = findViewById(R.id.signupbutton);
+
+        signup_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(MainActivity.this, SignUpCandidate.class);
+                startActivity(i);
+            }
+        });
     }
 }
 
--- GestInterim/app/src/main/java/com/example/gestinterim/SignUpCandidate.java
+++ GestInterim/app/src/main/java/com/example/gestinterim/SignUpCandidate.java
@@ -3,12 +3,67 @@ package com.example.gestinterim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpCandidate extends AppCompatActivity {
+
+    EditText firstName;
+    EditText lastName;
+    EditText city;
+    EditText nationality;
+    EditText phone_nr;
+    EditText email;
+    Button signup_button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_candidate);
+
+        firstName = findViewById(R.id.firstname);
+        lastName = findViewById(R.id.lastname);
+        city = findViewById(R.id.city);
+        nationality = findViewById(R.id.nationality);
+        phone_nr = findViewById(R.id.PhoneNumber);
+        email = findViewById(R.id.EMAIL);
+        signup_button = findViewById(R.id.signUp);
+
+        signup_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                checkData();
+            }
+        });
+    }
+
+    // check if data entered are correct
+    void checkData() {
+        if (isEmpty(firstName)) {
+            firstName.setError("First name is required!");
+        }
+
+        if (isEmpty(lastName)) {
+            lastName.setError("Last name is required!");
+        }
+
+        if (!isEmail(email)) {
+            email.setError("Enter valid email!");
+        }
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 }
