58feabeb75283b8dab7c61d4683fc05e0bfb2fab
Adele <56159423+Adeleimpa@users.noreply.github.com>
2023-04-14 12:45:39 +0200

    sign in mini debut
    je vais coder dans une branche àpd mtn pour eviter les conflits
    j'essaye de comprendre firebase là

--- GestInterim/app/src/main/java/com/example/gestinterim/MainActivity.java
+++ GestInterim/app/src/main/java/com/example/gestinterim/MainActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity { // Login activity
 
+    EditText email;
+    EditText password;
+    Button login_button;
     Button signup_button;
 
     @Override
@@ -17,6 +21,18 @@ public class MainActivity extends AppCompatActivity { // Login activity
         setContentView(R.layout.activity_main);
 
         signup_button = findViewById(R.id.signupbutton);
+        email = findViewById(R.id.mailinput);
+        password = findViewById(R.id.password);
+        login_button = findViewById(R.id.loginbutton);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO verify username and password are in database
+                //checkUsername();
+                //checkPassword();
+            }
+        });
 
         signup_button.setOnClickListener(new View.OnClickListener(){
             @Override
