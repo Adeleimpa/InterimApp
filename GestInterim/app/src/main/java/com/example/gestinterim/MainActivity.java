1e99c9cdc8e02735d3f2953d5bcc91f20bbf2758
Adele <56159423+Adeleimpa@users.noreply.github.com>
2023-04-23 19:29:22 +0200

    clean conflit

--- GestInterim/app/src/main/java/com/example/gestinterim/MainActivity.java
+++ GestInterim/app/src/main/java/com/example/gestinterim/MainActivity.java
@@ -7,62 +7,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-<<<<<<< Updated upstream
-
-public class MainActivity extends AppCompatActivity { // Login activity
-
-=======
-<<<<<<< HEAD
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity { // Login activity
 
-    Button signup_button, login_button;
-    EditText mail, password;
-=======
-
-public class MainActivity extends AppCompatActivity { // Login activity
-
->>>>>>> Stashed changes
     EditText email;
     EditText password;
     Button login_button;
     Button signup_button;
->>>>>>> 348c6976987641af5eda1e2a266093bf1325cf36
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //display welcome message
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    getSupportActionBar().setTitle("Welcome, " + user.getDisplayName() + "!");
+                } else {
+                    // in case null user
+                }
+            }
+        };
+
         signup_button = findViewById(R.id.signupbutton);
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
         login_button = findViewById(R.id.loginbutton);
-        mail = findViewById(R.id.mailinput);
-        password = findViewById(R.id.password);
-=======
->>>>>>> Stashed changes
         email = findViewById(R.id.mailinput);
         password = findViewById(R.id.password);
-        login_button = findViewById(R.id.loginbutton);
-
-        login_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO verify username and password are in database
-                //checkUsername();
-                //checkPassword();
-            }
-        });
-<<<<<<< Updated upstream
-=======
->>>>>>> 348c6976987641af5eda1e2a266093bf1325cf36
->>>>>>> Stashed changes
 
         signup_button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -75,9 +66,9 @@ public class MainActivity extends AppCompatActivity { // Login activity
         login_button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                String email = mail.getText().toString();
+                String email_str = email.getText().toString();
                 String pw = password.getText().toString();
-                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(pw)) {
+                if(TextUtils.isEmpty(email_str) || TextUtils.isEmpty(pw)) {
                     Toast.makeText(MainActivity.this, "Vous devez renseigner tous les champs !", Toast.LENGTH_SHORT).show();
                 }
                 else {
@@ -89,6 +80,43 @@ public class MainActivity extends AppCompatActivity { // Login activity
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
 
 }
 
