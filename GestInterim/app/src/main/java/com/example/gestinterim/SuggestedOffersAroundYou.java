6d9397ac4ed633ce2776596dd8a01183d70ffe1d
Adele <56159423+Adeleimpa@users.noreply.github.com>
2023-05-27 16:44:05 +0200

    list of job offers
    problem: only one item get display

--- GestInterim/app/src/main/java/com/example/gestinterim/SuggestedOffersAroundYou.java
+++ GestInterim/app/src/main/java/com/example/gestinterim/SuggestedOffersAroundYou.java
@@ -1,12 +1,17 @@
 package com.example.gestinterim;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SuggestedOffersAroundYou extends AppCompatActivity implements View.OnClickListener{
 
@@ -23,6 +28,16 @@ public class SuggestedOffersAroundYou extends AppCompatActivity implements View.
 
         goToMyProfile = findViewById(R.id.GoToMyProfile);
         goToMyProfile.setOnClickListener(this);
+
+        List<Job> jobsList = new ArrayList<>();
+        Job job1 = new Job("Dog sitter", "You will be asked to take care of a dog.", "Bxl", "company", "url");
+        Job job2 = new Job("Marius sitter", "You will be asked to take care of a Marius.", "Bxl", "company", "url");
+        jobsList.add(job1);
+        jobsList.add(job2);
+        RecyclerView recyclerView = findViewById(R.id.listJobs);
+        RecyclerView.Adapter<JobAdapter.ViewHolder> adapter = new JobAdapter(jobsList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
